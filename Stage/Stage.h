974a14ac0353e2b408f1570c68973cb92eca15dd
David Parton <david.parton@xome.com>
2016-08-01 11:30:52 -0700

    Initial Commit

---
+++ Stage/Stage.h
@@ -0,0 +1,19 @@
+//
+//  Stage.h
+//  Stage
+//
+//  Created by David Parton on 8/1/16.
+//  Copyright © 2016 deep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Stage.
+FOUNDATION_EXPORT double StageVersionNumber;
+
+//! Project version string for Stage.
+FOUNDATION_EXPORT const unsigned char StageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stage/PublicHeader.h>
+
+
